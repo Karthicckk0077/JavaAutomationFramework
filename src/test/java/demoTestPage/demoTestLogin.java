ff459b53d2917d40573383bb9e7bc474b1ccc28d
Karthik <karthiaerolife008@gmail.com>
2026-03-10 00:28:28 +0530

    The selenium grid dependency resolved

--- src/test/java/demoTestPage/demoTestLogin.java
+++ src/test/java/demoTestPage/demoTestLogin.java
@@ -6,7 +6,9 @@ import base.BaseTest;
 import org.example.waitclass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -33,25 +35,73 @@ public class demoTestLogin extends BaseTest {
 
 
 
-    @Parameters({"browser"})
-    @BeforeMethod
-    public void afterMethod(String browserName) throws MalformedURLException {
-        System.out.println("We are executing it on " + browserName);
+//    @Parameters({"browser"})
+//    @BeforeMethod
+//    public void afterMethod(String browserName) throws MalformedURLException {
+//        System.out.println("We are executing it on " + browserName);
+//
+//        switch (browserName) {
+//            case "chrome":
+//                chromeOptions = new ChromeOptions();
+//                driver = new RemoteWebDriver(new URL("http://192.168.29.31:4444"), chromeOptions);
+//                driver.get("https://demo.evershop.io/account/login");
+//                break;
+//
+//            case "edge":
+//                edgeOptions = new EdgeOptions();
+//                driver = new RemoteWebDriver(new URL("http://192.168.29.31:4444"), edgeOptions);
+//                driver.get("https://demo.evershop.io/account/login");
+//                break;
+//        }
+//    }
+@Parameters({"browser"})
+@BeforeMethod
+public void setUp(String browserName) throws Exception {
 
-        switch (browserName) {
+    boolean useGrid = Boolean.parseBoolean(System.getProperty("grid", "false"));
+    String gridUrl = System.getProperty("gridUrl", "http://localhost:4444/wd/hub");
+
+    System.out.println("Executing on browser: " + browserName);
+    System.out.println("Grid execution: " + useGrid);
+
+    if(useGrid){
+
+        switch (browserName){
+
             case "chrome":
                 chromeOptions = new ChromeOptions();
-                driver = new RemoteWebDriver(new URL("http://192.168.29.31:4444"), chromeOptions);
-                driver.get("https://demo.evershop.io/account/login");
+                driver = new RemoteWebDriver(new URL(gridUrl), chromeOptions);
                 break;
 
             case "edge":
                 edgeOptions = new EdgeOptions();
-                driver = new RemoteWebDriver(new URL("http://192.168.29.31:4444"), edgeOptions);
-                driver.get("https://demo.evershop.io/account/login");
+                driver = new RemoteWebDriver(new URL(gridUrl), edgeOptions);
                 break;
+
+            default:
+                throw new RuntimeException("Browser not supported: " + browserName);
+        }
+
+    } else {
+
+        switch (browserName){
+
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+
+            default:
+                throw new RuntimeException("Browser not supported: " + browserName);
         }
     }
+
+    driver.manage().window().maximize();
+    driver.get("https://demo.evershop.io/account/login");
+}
 
     @Test
     public void successLogin() throws MalformedURLException {
